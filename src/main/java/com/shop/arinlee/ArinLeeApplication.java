f73db35746063d645594f0e320eac3806cf47464
Arin0421 <arin9804@naver.com>
2022-07-11 16:01:19 +0900

    프로젝트 구조 생성

---
+++ src/main/java/com/shop/arinlee/ArinLeeApplication.java
@@ -0,0 +1,13 @@
+package com.shop.arinlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArinLeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArinLeeApplication.class, args);
+    }
+
+}
