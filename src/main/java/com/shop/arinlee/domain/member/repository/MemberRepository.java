ed2a1a3134a081eaabb88fb1cadfd24c092006ae
Arin0421 <arin9804@naver.com>
2022-07-14 16:29:54 +0900

    MemberRepository 생성 #2

---
+++ src/main/java/com/shop/arinlee/domain/member/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.shop.arinlee.domain.member.repository;
+
+import com.shop.arinlee.domain.member.Entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    
+}
