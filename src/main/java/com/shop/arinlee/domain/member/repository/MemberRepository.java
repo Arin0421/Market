de423764b388e606a6819c3e2e8f53da01d03d73
Arin0421 <arin9804@naver.com>
2022-07-14 16:32:11 +0900

    Member Entity 생성 #2

--- src/main/java/com/shop/arinlee/domain/member/repository/MemberRepository.java
+++ src/main/java/com/shop/arinlee/domain/member/repository/MemberRepository.java
@@ -4,5 +4,5 @@ import com.shop.arinlee.domain.member.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
-    
+    Member findByEmail(String email);
 }
