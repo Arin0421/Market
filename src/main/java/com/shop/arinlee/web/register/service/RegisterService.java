0d5c637f9bd730db2317f7f003f2829df02bd4a8
Arin0421 <arin9804@naver.com>
2022-07-15 15:33:18 +0900

    비밀번호 확인 #2

--- src/main/java/com/shop/arinlee/web/register/service/RegisterService.java
+++ src/main/java/com/shop/arinlee/web/register/service/RegisterService.java
@@ -2,6 +2,8 @@ package com.shop.arinlee.web.register.service;
 
 import com.shop.arinlee.domain.member.Entity.Member;
 import com.shop.arinlee.domain.member.service.MemberService;
+import com.shop.arinlee.global.error.exception.BusinessException;
+import com.shop.arinlee.global.error.exception.ErrorCode;
 import com.shop.arinlee.web.register.dto.RegisterFormDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,7 +17,16 @@ public class RegisterService {
     private final MemberService memberService;
 
     public void registerMember(RegisterFormDto registerFormDto){
+        validatePasswordCheck(registerFormDto);
         Member member = registerFormDto.toEntity();
         memberService.savedMember(member);
+    }
+
+    private void validatePasswordCheck(RegisterFormDto registerDto) {
+        String password1 = registerDto.getPassword();
+        String password2 = registerDto.getPassword2();
+        if (!password1.equals(password2)) {
+            throw new BusinessException(ErrorCode.MISMATCHED_PASSWORD);
+        }
     }
 }
