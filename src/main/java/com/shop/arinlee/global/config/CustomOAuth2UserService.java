6901c4d492b3f5fbb908467d2b000efb1bb817bb
Arin0421 <arin9804@naver.com>
2022-07-26 14:58:21 +0900

    OAuth2 관련 설정 추가 #9

---
+++ src/main/java/com/shop/arinlee/global/config/CustomOAuth2UserService.java
@@ -0,0 +1,45 @@
+package com.shop.arinlee.global.config;
+
+
+import com.shop.arinlee.domain.member.Entity.Member;
+import com.shop.arinlee.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint()
+                .getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        Member member = saveOrUpdate(attributes);
+
+        return UserDetailsImpl.create(member, attributes.getAttributes());
+
+    }
+
+    private Member saveOrUpdate(OAuthAttributes attributes){
+        Member member = memberRepository.findByEmail(attributes.getEmail())
+                .map(entity->entity.update(attributes.getName()))
+                .orElse(attributes.toEntity());
+
+        return memberRepository.save(member);
+
+    }
+}
