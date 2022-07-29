e40d5690cd972403c7994c07fe77775a12093091
Arin0421 <arin9804@naver.com>
2022-07-29 18:45:36 +0900

    feat : 수정 항목 유효성 검증 #12

--- src/main/java/com/shop/arinlee/web/profile/dto/ProfileUpdateDto.java
+++ src/main/java/com/shop/arinlee/web/profile/dto/ProfileUpdateDto.java
@@ -6,13 +6,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class ProfileUpdateDto {
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
 
+    @NotBlank(message = "주소는 필수 입력 값입니다.")
     private String address;
 
     @Builder
