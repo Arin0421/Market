472b3056b2eb94843d3325cadce8c62c2adefde5
Arin0421 <arin9804@naver.com>
2022-07-29 18:30:02 +0900

    test : Profile Controller 테스트 코드 작성 #12

---
+++ src/test/java/com/shop/arinlee/web/profile/controller/ProfileControllerTest.java
@@ -0,0 +1,102 @@
+package com.shop.arinlee.web.profile.controller;
+
+import com.shop.arinlee.domain.member.Entity.Member;
+import com.shop.arinlee.web.profile.service.ProfileService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.security.Principal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+public class ProfileControllerTest {
+
+    @InjectMocks
+    private ProfileController target;
+
+    @Mock
+    ProfileService profileService;
+
+    @Mock
+    private Principal principal;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init(){
+        mockMvc = MockMvcBuilders.standaloneSetup(target)
+                .build();
+    }
+
+    @Test
+    public void 프로필뷰반환테스트() throws Exception {
+        //given
+        when(principal.getName()).thenReturn("test");
+        when(profileService.getMemberInfo("test")).thenReturn(Member.builder().build());
+        final String url = "/profile";
+        final String view = "/profile/profileform";
+
+        //when
+        ResultActions resultActions = mockMvc.perform(get(url).principal(principal)).andDo(print());
+
+        //then
+        resultActions
+                .andExpect(model().attributeExists("profileUpdateDto"))
+                .andExpect(view().name(view));
+    }
+
+
+    @Test
+    public void 회원정보수정테스트_실패() throws Exception {
+        //given
+        final String url = "/profile";
+        final String view = "profile/profileform";
+        String name = null;
+        String address = "address";
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                post(url)
+                        .param("name", name)
+                        .param("address", address)
+        ).andDo(print());
+
+        //then
+        resultActions.andExpect(view().name(view));
+    }
+
+    @Test
+    public void 회원정보수정테스트_성공() throws Exception {
+        //given
+        when(principal.getName()).thenReturn("test");
+        final String url = "/profile";
+        final String view = "redirect:/";
+        String name = "name";
+        String address = "address";
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                post(url)
+                        .principal(principal)
+                        .param("name", name)
+                        .param("address", address)
+        ).andDo(print());
+
+        //then
+        resultActions.andExpect(view().name(view));
+    }
+}
