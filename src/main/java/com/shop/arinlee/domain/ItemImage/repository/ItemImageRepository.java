a1ef2dff4d1d31b42866e3e9672e26cf4d55c6b5
Arin0421 <arin9804@naver.com>
2022-08-08 20:17:51 +0900

    feat : ItemImage Repository 구현 #15

---
+++ src/main/java/com/shop/arinlee/domain/ItemImage/repository/ItemImageRepository.java
@@ -0,0 +1,7 @@
+package com.shop.arinlee.domain.ItemImage.repository;
+
+import com.shop.arinlee.domain.ItemImage.entity.ItemImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImageRepository extends JpaRepository<ItemImage,Long> {
+}
