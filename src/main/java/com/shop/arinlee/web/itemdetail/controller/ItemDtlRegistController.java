b3186e09a29cace4c145c1d66226ef43a637c1ea
Arin0421 <arin9804@naver.com>
2022-08-30 22:05:04 +0900

    feat : 주문 등록 Controller 구현 #21

---
+++ src/main/java/com/shop/arinlee/web/itemdetail/controller/ItemDtlRegistController.java
@@ -0,0 +1,27 @@
+package com.shop.arinlee.web.itemdetail.controller;
+
+import com.shop.arinlee.web.itemdetail.dto.RegisterOrderDto;
+import com.shop.arinlee.web.itemdetail.service.ItemDtlService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/itemdtl")
+public class ItemDtlRegistController {
+    private final ItemDtlService itemDtlService;
+
+    @PostMapping("/order")
+    public ResponseEntity registerOrderItem(@RequestBody RegisterOrderDto registerOrderDto, Principal principal) throws Exception {
+        itemDtlService.registerOrderItem(registerOrderDto, principal);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
