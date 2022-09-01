75c953ad4b9ca5f508ee5ce52ad6a40ed716e1fc
Arin0421 <arin9804@naver.com>
2022-09-01 16:20:42 +0900

    style : 오류 메세지 수정 #21

--- src/main/java/com/shop/arinlee/global/error/exception/StockException.java
+++ src/main/java/com/shop/arinlee/global/error/exception/StockException.java
@@ -2,6 +2,6 @@ package com.shop.arinlee.global.error.exception;
 
 public class StockException extends RuntimeException {
     public StockException(int stockNumber) {
-        super(ErrorCode.NOT_ENOUGH_STOCK.getMessage() + "(현재 재고 수량" + stockNumber + ")");
+        super(ErrorCode.NOT_ENOUGH_STOCK.getMessage() + "(현재 재고 수량 : " + stockNumber + ")");
     }
 }
