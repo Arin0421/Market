8c924557084913e10b3ba16d5b820f17689655a6
Arin0421 <arin9804@naver.com>
2022-09-08 17:02:48 +0900

    feat : Order Service 주문 이력 조회 기능 구현 #25

--- src/main/java/com/shop/arinlee/domain/order/service/OrderService.java
+++ src/main/java/com/shop/arinlee/domain/order/service/OrderService.java
@@ -4,7 +4,10 @@ import com.shop.arinlee.domain.member.Entity.Member;
 import com.shop.arinlee.domain.order.entity.Order;
 import com.shop.arinlee.domain.order.entity.OrderItem;
 import com.shop.arinlee.domain.order.repository.OrderRepository;
+import com.shop.arinlee.web.orderhist.dto.OrderHistDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +25,11 @@ public class OrderService {
     public void registerOrder(Member member, List<OrderItem> orderItemList) {
         Order order = Order.createOrder(member, orderItemList, LocalDateTime.now());
         ordersRepository.save(order);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderHistPage(Member member, Pageable pageable) {
+        return ordersRepository.getOrderHistByMember(member, pageable);
     }
 
 }
